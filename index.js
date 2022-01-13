e6f5a87348ccdfb3604cda13b664d84f510e8498
Lindsey <lindsey.pregent@gmail.com>
2022-01-13 17:35:56 -0500

    commit tree 0165bc3c5cb94996f07f1eeaee4d672fe4e7fee7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Lindsey',
+    streetAddress: '1609 8th Street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+//mutate the original array to update key/value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+//non destructively delete key/value
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = { ...employee }
+    delete newObj[key];
+    return newObj;
+}
+
+//destructively delete 
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+
+    return employee
+}
